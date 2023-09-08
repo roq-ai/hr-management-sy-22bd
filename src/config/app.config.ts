2bc74c94b4c9be509a4542a6228d7595ae8d9b57
roq-ai <>
2023-09-08 09:40:09 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,20 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: [],
+  tenantRoles: ['Owner', 'HR Manager'],
+  tenantName: 'Organization',
+  applicationName: 'HR Management System',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: ['Manage the organization in the system', 'Invite HR Managers to the application'],
+};
